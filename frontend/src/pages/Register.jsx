a4d180ac61f7547cf2e62f9c35814e7654617afd
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-05-18 22:30:17 +0800

    add register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -8,6 +8,7 @@ import {
   FaGoogle,
 } from "react-icons/fa";
 import { Link } from "react-router";
+import { MdEmail } from "react-icons/md";
 
 const Register = () => {
   const [password, setPassword] = useState("");
@@ -22,6 +23,29 @@ const Register = () => {
               Create your account
             </p>
             <form className="space-y-6 mt-6">
+              {/* Name Field */}
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-800"
+                >
+                  Full Name
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <FaUser size={18} className="text-gray-600" />
+                  </div>
+                  <input
+                    id="name"
+                    name="name"
+                    type="name"
+                    autoComplete="name"
+                    required
+                    className="block w-full pl-10 py-3 border border-gray-300 rounded-md bg-white text-gray-800 focus:ring-memorial-purple focus:border-memorial-purple/80"
+                    placeholder="Enter Full Name "
+                  />
+                </div>
+              </div>
               {/* Email Field */}
               <div>
                 <label
@@ -32,7 +56,7 @@ const Register = () => {
                 </label>
                 <div className="mt-1 relative rounded-md shadow-sm">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <FaUser size={18} className="text-gray-600" />
+                    <MdEmail size={18} className="text-gray-600" />
                   </div>
                   <input
                     id="email"
@@ -83,23 +107,70 @@ const Register = () => {
                   </div>
                 </div>
               </div>
-
-              {/* Remember Me */}
-              <div className="flex items-center justify-between">
-                <label className="flex items-center text-sm text-gray-800">
-                  <input
-                    id="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 text-royal-blue border-gray-300 rounded"
-                  />
-                  <span className="ml-2">Remember me</span>
-                </label>
-                <Link
-                  to="/forgot-password"
-                  className="text-sm text-rosewood hover:underline"
+              {/* Confirm Password */}
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-deep-charcoal"
                 >
-                  Forgot password?
-                </Link>
+                  Confirm Password
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <FaLock size={18} className="text-gray-600" />
+                  </div>
+                  <input
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full pl-10 pr-10 py-3 border border-gray-300 rounded-md bg-white text-gray-800 focus:ring-memorial-purple focus:border-memorial-purple/80"
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="text-gray-800 hover:text-gray-600"
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash size={18} />
+                      ) : (
+                        <FaEye size={18} />
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              {/* Privacy Policy */}
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    name="terms"
+                    type="checkbox"
+                    required
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label htmlFor="terms" className=" text-gray-800">
+                    I agree to the{" "}
+                    <Link to="/terms" className="text-rosewood hover:underline">
+                      Terms of Service
+                    </Link>{" "}
+                    and{" "}
+                    <Link
+                      to="/privacy"
+                      className="text-rosewood hover:underline"
+                    >
+                      Privacy Policy
+                    </Link>
+                  </label>
+                </div>
               </div>
 
               {/* Submit Button */}
@@ -109,14 +180,14 @@ const Register = () => {
                   isLoading ? "opacity-70 cursor-not-allowed" : ""
                 }`}
               >
-                {isLoading ? "Signing in..." : "Sign in"}
+                {isLoading ? "Signing in..." : "Create Account"}
               </button>
             </form>
             <div className="mt-6">
               <p className="text-sm text-gray-800 text-center">
-                Don't have an account?{" "}
+                Already have an account?{" "}
                 <span className=" text-rosewood hover:underline hover:cursor-pointer">
-                  Create one
+                  Sign in
                 </span>
               </p>
             </div>
